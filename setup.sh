868ff9de4988f93c2263ed6aaf3138a89ade8049
No Name <noname@null.com>
2020-05-09 05:26:30 +0000

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
-sudo apt install gcc
-sudo apt install make
-echo assumming /dev/sr0. To be sure, use cmd 'lsblk'
-echo INSURE GUEST ADDITIONS CD INSTALLED
-sudo rm -rf /mnt/sr0
-sudo mkdir /mnt/sr0
-sudo mount /dev/sr0 /mnt/sr0
-ls /mnt/sr0
-sudo /mnt/sr0/VBoxLinuxAdditions.run
-
+for fname in $(ls -1 *); do
+	echo $fname
+done
