6ba16153b0451135f91bbdbc33fc92fba001b0b4
Bryan Hann <nobody@nowhere>
2020-05-26 09:42:36 +0800

    BugFix

--- shared_folder.sh
+++ shared_folder.sh
@@ -3,10 +3,10 @@ _SHARENAME=host4ubuntu
 _HOSTPATH=/__shared/ubuntu
 _MOUNTPOINT=/mnt/${_SHARENAME}
 if [ "$1" == "--host" ]; then
-    echo VBoxManage sharedfolder add ${_GUEST_MACHINE_NAME} --name ${_SHARENAME} --hostpath ${_HOSTPATH}
+    VBoxManage sharedfolder add ${_GUEST_MACHINE_NAME} --name ${_SHARENAME} --hostpath ${_HOSTPATH}
 fi
 
-if [ "$1" == "--guest" ]; then 
-    echo sudo mkdir ${_MOUNTPOINT}
-    echo sudo mount -t vboxsf -o uid=1000,gid=1000 ${_SHARENAME} ${_MOUNTPOINT}
+if [ "$1" == "--guest" ]; then
+    sudo mkdir ${_MOUNTPOINT}
+    sudo mount -t vboxsf -o uid=1000,gid=1000 ${_SHARENAME} ${_MOUNTPOINT}
 fi
