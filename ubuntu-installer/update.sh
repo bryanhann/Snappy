c72191dcfd6a4bcf04718ad4968f536bd4b9df85
No Name <noname@null.com>
2020-08-08 03:14:01 +0000

    Refactor installer.

--- ubuntu-installer/update.sh
+++ ubuntu-installer/update.sh
@@ -1,24 +1,26 @@
-LOGDIR=~/.cache/log.install
-LOG=$LOGDIR/$( date "+%Y-%m-%d-T-%H:%M:%S" )
-mkdir -p $LOGDIR
-rm -rf ./typescript
+#!/bin/bash
+if [ "$1" == "" ]; then
+    export HERE=$(dirname $0)
+    export LOGDIR=~/.logs/SNAPPY-INSTALL-LOGS
+    mkdir -p $LOGDIR
+    export LOG=$LOGDIR/$( date "+%Y-%m-%d-T-%H:%M:%S" )
+    #export LOG=$HERE/$( date "+%Y-%m-%d-T-%H:%M:%S" )
+    script -c "$0 --recur"  $LOG
+    exit
+ fi
 
-function main {
-        sudo rm -f /var/lib/dpkg/lock*
-        sudo rm -f /var/cache/apt/archives/lock*
-	script -c "echo sudo $*;sudo $*" 
-	cat typescript >> $LOG
-	echo >> $LOG
-	rm typescript;
+ main () {
+    sudo rm -f /var/lib/dpkg/lock*
+    sudo rm -f /var/cache/apt/archives/lock*
+    echo "--------[sudo $*]--------"
+    sudo $*
+    echo
+    echo
 }
-
-main  apt-get -y update
-main  apt     -y full-upgrade
-main  apt     -y autoremove
-main  apt-get -y upgrade
-main  apt-get -y check
-main  apt     -y install python-pip
-main  apt     -y install zsh
-
-
-
+echo
+echo '###############################################################################'
+echo date: $(date)
+echo git: $(git log --oneline -n1)
+echo '###############################################################################'
+echo
+source $HERE/script
